b6e4eaaa2fedb998c382a64a53f4949b635df849
Stephen Mathews <busysteve@gmail.com>
2015-11-08 20:29:33 -0500

    Added Result set support for one or more records per id and organized class tyoes.

--- dumbdb.cpp
+++ dumbdb.cpp
@@ -7,21 +7,30 @@ using namespace std;
 int main()
 {
 	
-	dumbdb_table  table;
-	typedef dumbdb_table::row_t row;
+	dumbdb::table  table;
+	typedef dumbdb::row_t row;
 
 	
 	table.insert_row( row{ "Hello", "You", "stank", "fool" }, 2);
 	table.insert_row( row{ "So", "long", "sucka", "tash" }, 2);
 	table.insert_row( row{ "Bruce", "Lee", "kung", "fu" }, 2);
 	table.insert_row( row{ "Crazy", "Ja", "big", "head" }, 2);
+	table.insert_row( row{ "Bruce", "Lee", "Game", "Death" }, 2);
 
-	typedef dumbdb_index::idx_t key;
-	dumbdb_table::id_t id;
+	typedef dumbdb::idx_t key;
+	dumbdb::id_t id;
 
 	id = table.get_row_id( key{"Bruce", "Lee"} );
 
-	cout << table[id][2] << endl << flush;
+	if( id > 0 )
+	{
+		dumbdb::result rs = table[id];
+		cout << rs.count() << endl;
+		for( dumbdb::id_t i=0; i < rs.count(); i++ )
+			cout << rs[i][2] << endl << flush;
+	}
+	
+	cout << endl;
 
 	table.store( "data_out.dat" );
 	
@@ -30,12 +39,24 @@ int main()
 	id = table.get_row_id( key{"Cool", "Dude"} );
 
 	if( id > 0 )
-		cout << table[id][2] << endl << flush;
+	{
+		dumbdb::result rs = table[id];
+		cout << rs.count() << endl;
+		for( dumbdb::id_t i=0; i < rs.count(); i++ )
+			cout << rs[i][2] << endl << flush;
+	}
+
+	cout << endl;
 
 	id = table.get_row_id( key{"Bruce", "Lee"} );
 
 	if( id > 0 )
-		cout << table[id][2] << endl << flush;
+	{
+		dumbdb::result rs = table[id];
+		cout << rs.count() << endl;
+		for( dumbdb::id_t i=0; i < rs.count(); i++ )
+			cout << rs[i][2] << endl << flush;
+	}
 
 	return 0;
 }
--- dumbdb.h
+++ dumbdb.h
@@ -2,198 +2,237 @@
 #ifndef __DUMBDB
 #define __DUMBDB
 
-#include <tuple>
 #include <map>
+#include <tuple>
 #include <vector>
 #include <string>
 #include <atomic>
 #include <fstream>
-#include <iostream>
 #include <sstream>
 #include <utility>
+#include <iostream>
 
 using namespace std;
 
-class dumbdb_index
+
+class dumbdb
 {
 public:
-	typedef unsigned long 	id_t;
-	typedef vector<string>	idx_t;
+	typedef unsigned long 			id_t;
+	typedef vector<string>    		row_t;
+	typedef multimap< id_t, row_t >	 	table_t;
+	typedef vector< row_t > 		result_t;
+	typedef row_t				idx_t;
+
+	class result;
+	class table;
+	class index;
 
 
-	dumbdb_index() : idx_id(0L)
-	{}
-
-	id_t lookup_id( tuple<string, string, string, string, string, string> tup )
+	class index
 	{
-		auto it = idx.find( tup );
+	public:
 
-		if( it == idx.end() )
-			return 0L;
 
-		return it->second;
-	}
+		index() : idx_id(0L)
+		{}
 
-	id_t insert_id( tuple<string, string, string, string, string, string> tup )
-	{
-		unsigned long id=0L;
-
-		id = get_next_id();
-
-		idx[ tup ] = id;
-
-		return id;
-	}
-
-	id_t get_next_id()
-	{
-		return ++idx_id;
-	}
-
-	void clear()
-	{
-		idx.clear();
-	}
-
-private:
-	map< tuple<string, string, string, string, string, string>, unsigned long > idx;
-	atomic<unsigned long> idx_id;
-};
-
-class dumbdb_table
-{
-public:
-
-	typedef dumbdb_index::id_t id_t;
-	typedef vector<string>    row_t;
-	
-
-	string get_data( id_t id, int offset )
-	{
-		return table[id][offset];
-	}
-
-	void push_data( id_t id, string data )
-	{
-		table[id].push_back( data );
-	}
-
-	vector<string>& operator[]( id_t id )
-	{
-		return get_row(id);
-	}
-
-	vector<string>& get_row( id_t id )
-	{
-		return table[id];
-	}
-
-	id_t insert_row( row_t data, unsigned int ix )
-	{
-		string temp("");
-
-		if( data.size() < ix )
-			return 0L;
-
-		auto tup = make_tuple( 
-				(ix > 0) ? data[0] : temp, 
-				(ix > 1) ? data[1] : temp, 
-				(ix > 2) ? data[2] : temp, 
-				(ix > 3) ? data[3] : temp, 
-				(ix > 4) ? data[4] : temp, 
-				(ix > 5) ? data[5] : temp  );
-
-		id_t id = idx.lookup_id( tup );
-
-		if( id == 0L )
-			id = idx.insert_id( tup );
-
-		table[id] = data;
-
-		return id;
-	}
-
-	id_t get_row_id( dumbdb_index::idx_t key )
-	{
-		int ix = key.size();
-		string temp("");
-
-		auto tup = make_tuple( 
-					 key[0], 
-				(ix > 1) ? key[1] : temp, 
-				(ix > 2) ? key[2] : temp, 
-				(ix > 3) ? key[3] : temp, 
-				(ix > 4) ? key[4] : temp, 
-				(ix > 5) ? key[5] : temp  );
-		id_t id = idx.lookup_id( tup );
-
-		return id;
-	}
-
-	void store( const char * filename )
-	{
-		ofstream ofs ( filename, std::ofstream::binary );
-
-		for( auto row : table )
+		id_t lookup_id( tuple<string, string, string, string, string, string> tup )
 		{
-			string str;
+			auto it = idx.find( tup );
 
-			for( auto field : row.second )
-			{
-				str += field + '~';
-			}
+			if( it == idx.end() )
+				return 0L;
 
-			str[str.length()-1] = '\n';
-
-			ofs << str;
+			return it->second;
 		}
 
-		ofs << flush;
+		id_t insert_id( tuple<string, string, string, string, string, string> tup )
+		{
+			unsigned long id=0L;
+
+			id = get_next_id();
+
+			idx[ tup ] = id;
+
+			return id;
+		}
+
+		id_t get_next_id()
+		{
+			return ++idx_id;
+		}
+
+		void clear()
+		{
+			idx.clear();
+		}
+
+	private:
+		map< tuple<string, string, string, string, string, string>, unsigned long > idx;
+		atomic<unsigned long> idx_id;
+	};
+
+
+	class result
+	{
+		friend table;
+
+		void add_row( dumbdb::row_t row )
+		{
+			results.push_back( row );
+		}
+
+	public:
+		typedef vector< dumbdb::row_t > result_t;
+
+		unsigned int count()
+		{
+			return results.size();
+		}
+
+		row_t& get_row( unsigned int x )
+		{
+			return results[x];
+		}
+
+		row_t& operator[]( unsigned int x )
+		{
+			return get_row( x );
+		}
+
+	private:
+		result_t results;
+	};
+
+
+	class table
+	{
+	public:
+
+		result operator[]( id_t id )
+		{
+			return get_row(id);
+		}
+
+		result get_row( id_t id )
+		{
+			result rs;
+			auto range = table.equal_range(id);
+
+			for( auto it = range.first; it != range.second; it++ )
+				rs.add_row( it->second );
+
+			return rs;
+		}
+
+		id_t insert_row( row_t data, unsigned int ix )
+		{
+			string temp("");
+
+			if( data.size() < ix )
+				return 0L;
+
+			auto tup = make_tuple( 
+					(ix > 0) ? data[0] : temp, 
+					(ix > 1) ? data[1] : temp, 
+					(ix > 2) ? data[2] : temp, 
+					(ix > 3) ? data[3] : temp, 
+					(ix > 4) ? data[4] : temp, 
+					(ix > 5) ? data[5] : temp  );
+
+			id_t id = idx.lookup_id( tup );
+
+			if( id == 0L )
+				id = idx.insert_id( tup );
+
+			table.insert( make_pair( id, data) );
+
+			return id;
+		}
+
+		id_t get_row_id( idx_t key )
+		{
+			int ix = key.size();
+			string temp("");
+
+			auto tup = make_tuple( 
+						 key[0], 
+					(ix > 1) ? key[1] : temp, 
+					(ix > 2) ? key[2] : temp, 
+					(ix > 3) ? key[3] : temp, 
+					(ix > 4) ? key[4] : temp, 
+					(ix > 5) ? key[5] : temp  );
+			id_t id = idx.lookup_id( tup );
+
+			return id;
+		}
+
+		void store( const char * filename )
+		{
+			ofstream ofs ( filename, std::ofstream::binary );
+
+			for( auto row : table )
+			{
+				string str;
+
+				for( auto field : row.second )
+				{
+					str += field + '~';
+				}
+
+				str[str.length()-1] = '\n';
+
+				ofs << str;
+			}
+
+			ofs << flush;
 		
-	}
+		}
 
-	void load( const char * filename, unsigned int ix )
-	{
-		string line;
-
-		clear();
-
-		ifstream ifs ( filename, std::ifstream::binary);
-		istringstream iss;
-		row_t row;
-
-		while( getline( ifs, line, '\n' ) )
+		void load( const char * filename, unsigned int ix )
 		{
-			row.clear();
-			iss.str( line );
-			string word;
-			while( getline( iss, word, '~' ) )
-			{
-				row.push_back(word);
-			}
+			string line;
 
-			iss.clear();		
+			clear();
 
-			if( row.size() > ix )
+			ifstream ifs ( filename, std::ifstream::binary);
+			istringstream iss;
+			row_t row;
+
+			while( getline( ifs, line, '\n' ) )
 			{
-				insert_row( row, ix );
+				row.clear();
+				iss.str( line );
+				string word;
+				while( getline( iss, word, '~' ) )
+				{
+					row.push_back(word);
+				}
+
+				iss.clear();		
+
+				if( row.size() > ix )
+				{
+					insert_row( row, ix );
+				}
 			}
 		}
-	}
 
-	void clear()
-	{
-		idx.clear();
-		table.clear();
-	}
+		void clear()
+		{
+			idx.clear();
+			table.clear();
+		}
 
 
-private:
-	map< id_t, vector<string> > table;
-	dumbdb_index idx;
+	private:
+		table_t table;
+		index idx;
+	};
+
 };
-
-
 
 
 #endif
+
